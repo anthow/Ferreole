1aca80c0948bf79e3360d50f3a2289b63424b992
anthow <anthonyenglebert@gmail.com>
2022-07-21 12:02:04 +0200

    minify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,56 @@
 module.exports = {
   siteMetadata: {
-      title: ``,
-    siteUrl: `https://www.yourdomain.tld`,
-    titleTemplate: "%s · The Real Hero",
-    description:"coucou",
-    url:"",
-    image:"",
-    twitterUsername:"",
-
-
+    title: ``,
+    siteUrl: `https://www.ferreole.be/`,
+    url: "https://www.ferreole.be/",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet",
-   "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, 
-  {
-    resolve: `gatsby-source-datocms`,
-    options: {
-      // You can find your read-only API token under the Settings > API tokens
-      // section of your administrative area. Make sure to grant both CDA and CMA permissions.
-      apiToken: `f541d2d892634ab251d957a6fa8638`,
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-minify`,
+    "gatsby-plugin-sitemap",
 
-      // The project environment to read from. Defaults to the primary environment:
-      environment: `main`,
-
-      // If you are working on development/staging environment, you might want to
-      // preview the latest version of records instead of the published one:
-      previewMode: false,
-
-      // Disable automatic reloading of content when some change occurs on DatoCMS:
-      disableLiveReload: false,
-
-      // Custom API base URL (most don't need this)
-      // apiUrl: 'https://site-api.datocms.com',
-
-      // Limits page size and can be used to avoid build timeouts.
-      // Default is 500
-      pageSize: 10,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-  },
-  
-  "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: `f541d2d892634ab251d957a6fa8638`,
+
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+
+        // Custom API base URL (most don't need this)
+        // apiUrl: 'https://site-api.datocms.com',
+
+        // Limits page size and can be used to avoid build timeouts.
+        // Default is 500
+        pageSize: 10,
+      },
     },
-    __key: "images"
-  }]
+
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
-
