f54fdc4c443e8414b784539d4caeffc835198870
anthow <anthonyenglebert@gmail.com>
2022-08-23 10:32:36 +0200

    newsletter

--- src/components/footer/sectionOne.js
+++ src/components/footer/sectionOne.js
@@ -3,15 +3,20 @@ import React from "react";
 const SectionOne = () => {
   return (
     <article className=" col-span-2  w-10/12">
-      <section className="hidden">
-      <h4 className="text-2xl font-black mb-5">Newsletter</h4>
-      <form className="flex w-max">
-        <input type="text" className="p-2" />
-        <input type="submit" value="envoyer" nom="envoyer" className=" border text-white p-2" />
-      </form>
+      <section className="">
+      <iframe width="540" height="600" 
+src="https://9af0456e.sibforms.com/serve/MUIEABwWLqlMaDn9lv1wdcRgzehGSUMI_2Qv9W-M4ntqyyggvOpzeogKbk06kOcgyAH3lU5HjxpdMzJttn8rsKFVk-ALQJrCw1twx65-G4lQ-UXe6lX_jCO9FZsDGdGZYZ2tfbF2fgtdEww1mr_LvlWVI24vNi5ecQGTTaR7O5a3ywQkapLdThyHjogDA_nb3UWH4gucLViyyWAX" 
+frameborder="0" scrolling="auto" allowfullscreen 
+    styles="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"
+    className="-mt-20"
+
+    >
+
+    </iframe>
       </section>
     </article>
   );
 };
 
 export default SectionOne;
+
